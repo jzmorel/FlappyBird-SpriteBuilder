f78174d6d7eea2e75757ae9dd2652a3e2f96aa7c
jzmorel <jzmorel@JZs-MacBook-Pro.local>
2014-11-10 18:29:08 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -18,6 +18,13 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
     // define variables here;
+    Character* character;       // will store Flappy Bird instance
+    CCPhysicsNode* physicsNode; // will reference a node that simulates physics
+                                // a "node" is a single unit in a hierarchy (kind of like OO inheritance)
+                                // a parent node can have 1 or more child nodes
+                                // child nodes are influenced by parent node, but parent isn't influenced by child
+                                // earth <- car <- driver, if move car, it updates position of driver, but not earth
+    
 }
 
 -(void) initialize;
