d16ded7d36f4f05d4669c77b77cecd63f1b5d470
jzmorel <jzmorel@JZs-MacBook-Pro.local>
2014-11-10 20:16:08 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.h
+++ FlappyBird.spritebuilder/Source/GamePlayScene.h
@@ -17,14 +17,14 @@ typedef NS_ENUM(NSInteger, DrawingOrder) {
 
 @interface GamePlayScene : CCNode <CCPhysicsCollisionDelegate>
 {
-    // define variables here;
+    // define variables here;  (aka declare variables?)
     Character* character;       // will store Flappy Bird instance
     CCPhysicsNode* physicsNode; // will reference a node that simulates physics
                                 // a "node" is a single unit in a hierarchy (kind of like OO inheritance)
                                 // a parent node can have 1 or more child nodes
                                 // child nodes are influenced by parent node, but parent isn't influenced by child
                                 // earth <- car <- driver, if move car, it updates position of driver, but not earth
-    
+    float timeSinceObstacle;
 }
 
 -(void) initialize;
--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,28 @@
     character = (Character*)[CCBReader load:@"Character"]; //initializes the object "character" with data from the CCB file called "Character"
     [physicsNode addChild:character];   // adds object "character" to be a child of physicsNode
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    // put update code here.  in Cocoa2D, default update loop runs 60 times per second
+    // this code will be run every frame.
+    // delta is the time that has elapsed since the last time it was run.
+    // This is usually 1/60, but can be bigger if the game slows down.
+    
+    // Increment the time since the last obstacle was added
+    timeSinceObstacle += delta;  // delta is approximately 1/60th of a second
+    
+    // Check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        // Add a new obstacle
+        [self addObstacle];
+        
+        // reset the timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
