9272b45ed20e69663cb1bce10335bf09b57802e6
jzmorel <jzmorel@JZs-MacBook-Pro.local>
2014-11-10 19:38:37 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,8 +7,8 @@
 - (void)initialize
 {
     // your code here
-    character = (Character*)[CCBReader load:@"Character"];
-    [physicsNode addChild:character];
+    character = (Character*)[CCBReader load:@"Character"]; //initializes the object "character" with data from the CCB file called "Character"
+    [physicsNode addChild:character];   // adds object "character" to be a child of physicsNode
 }
 
 -(void)update:(CCTime)delta
@@ -17,5 +17,8 @@
 }
 
 // put new methods here
-
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // get called automatically every time the player touches the screen
+    [character flap]; // applies a 'fly upward' impulse to "character", simulated by physicsNode
+}
 @end
